3fa76432810da851d968f59f9177ec13377b4ea6
kwonsw812 <kwonsw812@gmai.com>
2023-07-29 01:42:13 +0900

    Mappedsuperclass and inheritence

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop;
 
 import com.sun.org.apache.xpath.internal.operations.Or;
+import jpabook.jpashop.domain.Book;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
@@ -20,6 +21,11 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("김영한");
+
+            entityManager.persist(book);
 
             tx.commit();
         } catch (Exception e) {
